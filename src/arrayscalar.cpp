c5119d6d8a1ccd017e007caf150c6e423406e692
Ben Everman <94769250+beverm2391@users.noreply.github.com>
2024-02-02 13:06:56 -0500

    updates to arrayscalar

--- src/arrayscalar.cpp
+++ src/arrayscalar.cpp
@@ -3,13 +3,14 @@
 
 enum class DataType
 {
+    float16,
     float32,
-    float64,
+    // float64,
     // int8,
     // int16,
     int32,
     // int64,
-    // uint8, 
+    // uint8,
     // uint16,
     // uint32,
     // uint64,
@@ -27,41 +28,64 @@ private:
 public:
     Dtype(DataType type, size_t size) : type(type), size(size) {}
 
-    DataType getType() const { return type; }
-    size_t getSize() const { return size; }
-
-    // TODO add more methods
+    DataType getType() const
+    {
+        return type;
+    }
+    size_t getSize() const
+    {
+        switch (type)
+        {
+        case DataType::float16:
+            return 2;
+        case DataType::float32:
+            return 4;
+        case DataType::int32:
+            return 4;
+        default:
+            throw std::runtime_error("Unsupported data type: " + std::to_string(static_cast<int>(type)));
+        }
+        // TODO add more methods
+    };
 };
 
 class ArrayScalar
 {
-    private:
+private:
     double value;
     Dtype dtype;
 
-    public:
-        ArrayScalar(double value, Dtype dtype) : value(value), dtype(dtype) {}
+public:
+    ArrayScalar(double value, Dtype dtype) : value(value), dtype(dtype) {}
 
-        double getValue() const { return value; }
-        Dtype getDtype() const { return dtype; }
+    double getValue() const { return value; }
+    Dtype getDtype() const { return dtype; }
 
-        // TODO add more methods
+    // TODO add more methods
 };
-
-
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(teenyarray, m) {
+PYBIND11_MODULE(teenyarray, m)
+{
+    // define the DataType enum
+    py::enum_<DataType>(m, "DataType")
+        .value("float16", DataType::float16)
+        .value("float32", DataType::float32)
+        .value("int32", DataType::int32)
+        .export_values(); // export the enum values to Python
+
+    // define the Dtype class
     py::class_<Dtype>(m, "Dtype")
         .def(py::init<std::string, size_t>())
-        .def("getName", &Dtype::getName)
+        .def("getType", &Dtype::getType)
         .def("getSize", &Dtype::getSize);
-        // TODO add more methods
+    // TODO add more methods
 
+    // define the ArrayScalar class
     py::class_<ArrayScalar>(m, "ArrayScalar")
         .def(py::init<double, Dtype>())
         .def("getValue", &ArrayScalar::getValue)
         .def("getDtype", &ArrayScalar::getDtype);
-        // TODO add more methods
+    // TODO add more methods
 }
